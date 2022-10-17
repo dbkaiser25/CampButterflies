4b4892abf95564ae6568888985e7835993f31d7e
dbkaiser25 <derekkaiser133@gmail.com>
2022-10-17 08:33:09 -0400

    updating week and calendar

--- Week.java
+++ Week.java
@@ -1,8 +1,37 @@
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Week 
 {
-    //TODO write this class
+    //TODO figure out the dimension labels of schedule/masterschedule hashmap
+    private HashMap<Group, ArrayList<Activity>> schedule; 
+    private ArrayList<Group> groups;
+    private ArrayList<Counselor> counselors;
+    private ArrayList<Camper> campers;
+    private Date startDate;
+    private Date endDate;
+    private boolean isFull;
 
-    //private HashMap<, 
+    //TODO write constructor
+    public Week()
+    {
+        //Constructor goes here
+    }
+
+    public void generateSchedule()
+    {
+        //do schedule things here
+    }
+
+    public String viewSchedule()
+    {
+        //do more schedule things here (related to viewing)
+        return null;
+    }
+
+    public void editSchedule()
+    {
+        //edit da schedule here
+    }
 }
