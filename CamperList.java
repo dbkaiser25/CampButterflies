44453def56f1c3d741bf5e80deac50ef27a97fe9
dbkaiser25 <derekkaiser133@gmail.com>
2022-10-18 13:34:58 -0400

    starting list classes

---
+++ CamperList.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+public class CamperList 
+{
+   private ArrayList<Camper> campers;
+   private static CamperList camperList;
+   
+   
+   private CamperList()
+   {
+
+   }
+
+   public CamperList getInstance()
+   {
+        return null;
+   }
+
+   public void addCamper(Camper camper)
+   {
+
+   }
+
+   public Camper getCamper()
+   {
+        return null;
+   }
+
+   public void editCamper()
+   {
+
+   }
+
+   public void saveCamper()
+   {
+    
+   }
+}
