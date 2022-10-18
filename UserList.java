db61f97f36f2d873069aed413a03f8a1e4b368f6
Vershon Lee <vershonlee@Vershons-MacBook-Pro-2.local>
2022-10-18 14:08:21 -0400

    creating Dirlist,Userlist, counselorList

--- UserList.java
+++ UserList.java
@@ -2,8 +2,24 @@ import java.util.ArrayList;
 
 public class UserList {
     private ArrayList<User> users;
-    private UserList userList;
+    private static UserList userList;
 
 private UserList(){
+    // constructor foes here
+}
+public static UserList getInstance(){
+    return null;
+}
+public void addUser(User user){
+    // add user
+}
+public User getUser(){
+    return null;
+}
+public void editUser(){
+
+}
+public void saveUsers(){
+    
 }
 }
