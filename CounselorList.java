4aabc20249c86baa27f9a47be9a9e7c392d5a549
dbkaiser25 <derekkaiser133@gmail.com>
2022-10-20 13:47:45 -0400

    starting counselor list functionality

--- CounselorList.java
+++ CounselorList.java
@@ -1,27 +1,56 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class CounselorList 
 {
-    private ArrayList<Counselor> conselors;
+    private ArrayList<Counselor> counselors;
+    //private ArrayList<Counselor> conselors = new ArrayList();
     private static CounselorList counselorList;
 
     private CounselorList()
     {
+        counselors = new ArrayList<Counselor>();
+        //counselors = new ArrayList<Counselor>();
         // add constructor
     }
 
     public static CounselorList getInstance()
     {
+        if(counselorList == null)
+        {
+            counselorList = new CounselorList();
+        }
+        return counselorList;
+    }
+
+    /* IDK if we need this or not
+    public Counselor getCounselorUUID(UUID uuid)
+    {
+
         return null;
     }
+    */
 
-    public void addCounselor(Counselor counselor)
+    public boolean addCounselor(Counselor counselor)
     {
-
+        if(counselor == null)
+        {
+            return false;
+        }
+        else
+        {
+            counselors.add(counselor);
+            return true;
+        }
     }
 
-    public Counselor getCounselor()
+    
+    public Counselor getCounselorByUsername(String userName)
     {
+        for(Counselor c: counselors)
+        {
+            //if
+        }
         return null;
     }
 
