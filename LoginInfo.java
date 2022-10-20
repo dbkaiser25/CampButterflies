90b0c3fc3ae15044e7b0bc5a98679413f8ff94f7
Vershon Lee <vershonlee@Vershons-MacBook-Pro-2.local>
2022-10-20 13:51:02 -0400

    adding to logininfo

--- LoginInfo.java
+++ LoginInfo.java
@@ -17,4 +17,20 @@ public class LoginInfo
         this.userName = userName;
         this.password = password;
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
