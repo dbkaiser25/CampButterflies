cf51eb291bec856421ad04ed4a29aaf4e53f6c6e
Vershon Lee <vershonlee@Vershons-MacBook-Pro-2.local>
2022-10-21 12:27:30 -0400

    getters and setters

--- Contact.java
+++ Contact.java
@@ -36,6 +36,46 @@ public class Contact
 
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getRelationToPerson() {
+        return relationToPerson;
+    }
+
+    public void setRelationToPerson(String relationToPerson) {
+        this.relationToPerson = relationToPerson;
+    }
+
     /**
      * A pretty description of the Contact in string form
      * @return A string for the description of the contact
